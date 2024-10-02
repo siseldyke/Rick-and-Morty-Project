102e1a6b97e57930aa36b905cabe16fe22c44f50
siseldyke <siseldyke@gmail.com>
2024-10-02 19:59:19 -0400

    text hidden until search now

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const nameContainer = document.querySelector('#charName')
 const titleContainer = document.querySelector('#charSpecies')
 const imageContainer = document.querySelector('#charImage')
 const statusContainer = document.querySelector('#charStatus')
-const statShow = document.querySelector('.statImageShow')
+// const statShow = document.querySelector('.statImageShow')
 const locationContainer = document.querySelector('#charLocation')
 const originContainer = document.querySelector('#charOrigin')
 const nextButton = document.querySelector('#nextChar')
 const prevButton = document.querySelector('#previousChar')
 const soundButton = document.querySelector('#soundButton')
-
-
+const fullText = document.querySelector('#statSheet', '#locationData')
+const speciesTitle = document.querySelector('#Species')
+const statusTitle = document.querySelector('#status')
+const locationTitle = document.querySelector('#Location')
+const originTitle = document.querySelector('#Origin')
 // functions
 
 
@@ -25,21 +28,21 @@ button.addEventListener('click',  async () => {
     let response = await axios.get(
         `https://rickandmortyapi.com/api/character/?name=${name}`
     )
-    // if (input.key === 'Enter'){
-    //     input.preventDefault()
-    //     document.querySelector('#submitButton').click()
-    // }
-    console.log(response)
+   console.log(response.data.results[0])
+    
 
     let charName = response.data.results[0].name
     nameContainer.textContent = (charName)
     
     let charSpecies = response.data.results[0].species
-    titleContainer.textContent=(charSpecies)
+    titleContainer.textContent= (charSpecies)
+
+    speciesTitle.textContent =`Species: `
 
     let charStatus = response.data.results[0].status
     statusContainer.textContent=(charStatus)
    
+    statusTitle.textContent =`Status: `
     // if(charName === name){
     //     statShow.classList.add('.statImageShow')
     // }
@@ -47,8 +50,12 @@ button.addEventListener('click',  async () => {
     let charLocate = response.data.results[0].location.name
     locationContainer.textContent = (charLocate)
 
+    locationTitle.textContent =`Current Location: `
+
     let charOrigin = response.data.results[0].origin.name
     originContainer.textContent = (charOrigin)
+
+    originTitle.textContent =`Origin: `
 
     let charImage = response.data.results[0].image
     imageContainer.setAttribute('src', charImage)
@@ -59,21 +66,19 @@ button.addEventListener('click',  async () => {
     
     nextButton.addEventListener('click' , async() =>{
         currentIndex = (currentIndex + 1)
-        console.log(currentIndex)
         nameContainer.textContent = (response.data.results[currentIndex].name)
         titleContainer.textContent = (response.data.results[currentIndex].species)
         statusContainer.textContent = (response.data.results[currentIndex].status)
         locationContainer.textcontent = (response.data.results[currentIndex].location.name)
         originContainer.textContent = (response.data.results[currentIndex].origin.name)
         imageContainer.setAttribute('src',  response.data.results[currentIndex].image)
-    })
-    // let prevIndex = 0    
+    })  
     prevButton.addEventListener('click', async() =>{
-       console.log(currentIndex)
+       
         if (currentIndex > 0){
             currentIndex = (currentIndex - 1)
         }
-        console.log(currentIndex)
+        
         nameContainer.textContent = (response.data.results[currentIndex].name)
         titleContainer.textContent = (response.data.results[currentIndex].species)
         statusContainer.textContent = (response.data.results[currentIndex].status)
