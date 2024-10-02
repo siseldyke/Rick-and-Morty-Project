0e9d4d92ace55dd15a4df0bdd759523aad7d5831
siseldyke <siseldyke@gmail.com>
2024-10-02 13:32:28 -0400

    working next character button

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const nameContainer = document.querySelector('#charName')
 const titleContainer = document.querySelector('#charSpecies')
 const imageContainer = document.querySelector('#charImage')
 const statusContainer = document.querySelector('#charStatus')
+const statShow = document.querySelector('.statImageShow')
 const locationContainer = document.querySelector('#charLocation')
 const originContainer = document.querySelector('#charOrigin')
 const nextButton = document.querySelector('#nextChar')
@@ -17,11 +18,15 @@ const prevButton = document.querySelector('#previousChar')
 
 // functions
 
-button.addEventListener('click', async () => {
+button.addEventListener('click',  async () => {
     let name = input.value
     let response = await axios.get(
         `https://rickandmortyapi.com/api/character/?name=${name}`
     )
+    // if (input.key === 'Enter'){
+    //     input.preventDefault()
+    //     document.querySelector('#submitButton').click()
+    // }
     console.log(response)
 
     let charName = response.data.results[0].name
@@ -32,6 +37,10 @@ button.addEventListener('click', async () => {
 
     let charStatus = response.data.results[0].status
     statusContainer.textContent=(charStatus)
+   
+    // if(charName === name){
+    //     statShow.classList.add('.statImageShow')
+    // }
 
     // let charLocate = response.data.results[0].location.name
     // locationContainer.textContent = (charLocate)
@@ -41,11 +50,24 @@ button.addEventListener('click', async () => {
 
     let charImage = response.data.results[0].image
     imageContainer.setAttribute('src', charImage)
+
+
+    let charIndex = response.data.results[0].id
+    let currentIndex = 0;
+    
+    nextButton.addEventListener('click' , async() =>{
+       console.log(currentIndex)
+        currentIndex = (currentIndex + 1)
+        console.log(charIndex)
+        console.log(response.data.results[currentIndex])
+        nameContainer.textContent = (response.data.results[currentIndex].name)
+        titleContainer.textContent = (response.data.results[currentIndex].species)
+        statusContainer.textContent = (response.data.results[currentIndex].status)
+        imageContainer.setAttribute('src',  response.data.results[currentIndex].image)
+        
+    })
 })
 
-    // let next = document.querySelector("next").textContent;
-    // let currentIndex =0;
-    // nextButton.addEventListener('click' , async() =>{
-    //     currentIndex = (currentIndex + 1) % next.length;
-    //     document.querySelector('next').textContent = next.charAt(currentIndex)
-    // })
+
+
+    
